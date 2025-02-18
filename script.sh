95aaa3ce1fe657be8d48ff39754fe39eeb989cef
shanuba <shanuba537@gmail.com>
2025-02-18 21:52:07 +0300

    second commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting Jenkins Freestyle Job with Bash"
+CURRENT_DATE=$(date)
+echo "The current date and time is: $CURRENT_DATE"
+echo "Generating a random number:"
+RANDOM_NUMBER=$((RANDOM % 100 + 1))
+echo "Random Number: $RANDOM_NUMBER"
+echo "Jenkins Job completed successfully "
