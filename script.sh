b4189c4d6b7829ffd44c993c824ff2dd6ff3f1bc
Shanuba-github <104367849+shanuba@users.noreply.github.com>
2025-02-19 14:25:57 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ echo "The current date and time is: $CURRENT_DATE"
 echo "Generating a random number:"
 RANDOM_NUMBER=$((RANDOM % 100 + 1))
 echo "Random Number: $RANDOM_NUMBER"
-echo "Jenkins Job completed successfully "
+echo "Jenkins Job completed successfully"
