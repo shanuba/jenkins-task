4f590dae518760f2a172d2465421ebec80f20814
Shanuba-github <104367849+shanuba@users.noreply.github.com>
2025-02-27 13:41:33 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 echo "Starting Jenkins Freestyle Job with Bash"
-CURRENT_DATE=$(date)
-echo "The current date and time is: $CURRENT_DATE"
-echo "Generating a random number:"
-RANDOM_NUMBER=$((RANDOM % 100 + 1))
-echo "Random Number: $RANDOM_NUMBER"
 echo "Jenkins Job completed successfully"
